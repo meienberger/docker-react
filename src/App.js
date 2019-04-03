30f9c8316a777272f2f413836fbbd5ce382b2856
Nicolas Meienberger <nicolas@iMac-de-Nicolas.local>
2019-04-03 16:14:17 +0200

    Changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>Bye there !</p>
+					<p>Docker life</p>
 				</header>
 			</div>
 		);
